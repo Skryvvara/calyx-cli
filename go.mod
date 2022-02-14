37ea7f9b55459ff177e16b94f4ab009bb6f2d38f
Dimitri Kaiser <darkyne.gaming@outlook.de>
2022-02-14 13:53:40 +0100

    feat: added flag to copy generated key to clipboard. closes #1

--- go.mod
+++ go.mod
@@ -8,4 +8,9 @@ require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	golang.design/x/clipboard v0.6.0 // indirect
+	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
+	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
+	golang.org/x/mobile v0.0.0-20210716004757-34ab1303b554 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 )
--- pkg/actions/generate.go
+++ pkg/actions/generate.go
@@ -3,12 +3,16 @@ package actions
 import (
 	"crypto/rand"
 	"fmt"
+	"log"
 
 	"github.com/urfave/cli/v2"
+	"golang.design/x/clipboard"
 )
 
 func Generate(c *cli.Context) error {
 	size := c.Int("bytes")
+	copy := c.Bool("copy")
+	fmt.Println(copy)
 	if size < 0 {
 		fmt.Println("Invalid amount of bytes, defaulting to 32.")
 		size = 32
@@ -20,6 +24,14 @@ func Generate(c *cli.Context) error {
 		return err
 	}
 	str := fmt.Sprintf("%x", key)
+	err = clipboard.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if copy == true {
+		clipboard.Write(clipboard.FmtText, []byte(str))
+	}
+
 	fmt.Println(str)
 	return nil
 }
